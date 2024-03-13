22d576ed96c845be2dc8e1f9f7cde2e22155a311
ttlam.dev <ttlam.dev@gmail.com>
2024-03-13 17:40:32 +0700

    feat: Adding hash password and email verification

--- app/src/main/java/hcmus/android/crm/MainActivity.java
+++ app/src/main/java/hcmus/android/crm/MainActivity.java
@@ -2,13 +2,67 @@ package hcmus.android.crm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
+
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+import hcmus.android.crm.activities.SignInActivity;
+import hcmus.android.crm.databinding.ActivityMainBinding;
+import hcmus.android.crm.utilities.Constants;
+import hcmus.android.crm.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        auth = FirebaseAuth.getInstance();
+        loadCredentials();
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.imageSignOut.setOnClickListener(v -> {
+            signOut();
+        });
+    }
+
+    private void loadCredentials() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+     /*   byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);*/
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signOut() {
+        showToast("Signing out...");
+        preferenceManager.clear();
+        auth.signOut();
+        startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+        finish();
     }
 }
--- app/src/main/java/hcmus/android/crm/utilities/Constants.java
+++ app/src/main/java/hcmus/android/crm/utilities/Constants.java
@@ -5,8 +5,11 @@ public class Constants {
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
     public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PHONE_NUMBER = "phoneNumber";
+
     public static final String KEY_PREFERENCE_NAME = "crmAppPreference";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public static final String KEY_USER_ID = "userId";
     public static final String KEY_IMAGE = "image";
+
 }
