ab90e8a7e8af6b58deac8f0f7207c5c4c9ccac21
Le Vu Ngan Lam <nganlamforwork@gmail.com>
2024-04-19 01:49:43 +0700

    feat: show tags for leads and edit tags

--- app/src/main/java/hcmus/android/crm/models/Tag.java
+++ app/src/main/java/hcmus/android/crm/models/Tag.java
@@ -3,17 +3,22 @@ package hcmus.android.crm.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.firebase.firestore.Exclude;
 import com.google.firebase.firestore.PropertyName;
 import com.google.firebase.firestore.ServerTimestamp;
 
 import java.util.Date;
 
 public class Tag implements Parcelable {
+    @Exclude // Exclude from Firestore serialization/deserialization
+    private String id;
     @PropertyName("title")
     private String title;
     @PropertyName("createdAt")
     @ServerTimestamp
-    private Date createdAt;public Tag() {
+    private Date createdAt;
+
+    public Tag() {
     }
 
     public Tag(String title) {
@@ -22,10 +27,12 @@ public class Tag implements Parcelable {
 
     // Parcelable implementation
     protected Tag(Parcel in) {
+        id = in.readString();
         title = in.readString();
         long tmpCreatedAt = in.readLong();
         createdAt = tmpCreatedAt == -1 ? null : new Date(tmpCreatedAt);
     }
+
     public static final Creator<Tag> CREATOR = new Creator<Tag>() {
         @Override
         public Tag createFromParcel(Parcel in) {
@@ -37,6 +44,14 @@ public class Tag implements Parcelable {
             return new Tag[size];
         }
     };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -58,8 +73,10 @@ public class Tag implements Parcelable {
     public int describeContents() {
         return 0;
     }
+
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
         dest.writeString(title);
         dest.writeLong(createdAt != null ? createdAt.getTime() : -1);
     }
