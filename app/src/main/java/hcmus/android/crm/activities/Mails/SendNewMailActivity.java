5ea72ac0b8dcc9219d591b14cbcb04d44ce906b9
tdakhoa <tdakhoa@TDAKHOA>
2024-04-25 12:02:23 +0700

    feat: send email with template

--- app/src/main/java/hcmus/android/crm/activities/Mails/SendNewMailActivity.java
+++ app/src/main/java/hcmus/android/crm/activities/Mails/SendNewMailActivity.java
@@ -1,5 +1,6 @@
 package hcmus.android.crm.activities.Mails;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,15 +8,28 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.List;
 
 import hcmus.android.crm.R;
 import hcmus.android.crm.activities.BusinessCard.BusinessCardActivity;
@@ -23,6 +37,8 @@ import hcmus.android.crm.activities.BusinessCard.EditBusinessCardActivity;
 import hcmus.android.crm.activities.DrawerBaseActivity;
 import hcmus.android.crm.databinding.ActivityAddNewBussinessCardAcitivityBinding;
 import hcmus.android.crm.databinding.ActivitySendNewMailBinding;
+import hcmus.android.crm.models.Template;
+import hcmus.android.crm.utilities.Constants;
 
 public class SendNewMailActivity extends DrawerBaseActivity {
     private static final int CHOOSE_EMAILS_REQUEST_CODE = 100;
@@ -32,6 +48,8 @@ public class SendNewMailActivity extends DrawerBaseActivity {
     private Button sendButton;
     private FirebaseFirestore db;
     private ArrayList<String> choosenEmails;
+    private AutoCompleteTextView templateDropdown;
+    private List<Template> templatesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +65,7 @@ public class SendNewMailActivity extends DrawerBaseActivity {
 
         db = FirebaseFirestore.getInstance();
         // Enable the back button in the action bar or toolbar
+        // setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null) {
@@ -54,6 +73,13 @@ public class SendNewMailActivity extends DrawerBaseActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowHomeEnabled(true);
         }
+        // Get UI elements
+        templateDropdown = binding.templateDropdown;
+
+        // Fetch templates from Firestore
+        fetchTemplates();
+
+        // Set listeners
         setListeners();
     }
 
@@ -89,6 +115,15 @@ public class SendNewMailActivity extends DrawerBaseActivity {
             @Override
             public void onClick(View v) {
                 onSendEmail();
+            }
+        });
+        templateDropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Handle selection of a template
+                Template selectedTemplate = templatesList.get(position);
+                emailBody.setText(selectedTemplate.getBody());
+                emailSubject.setText(selectedTemplate.getSubject());
             }
         });
     }
@@ -131,6 +166,43 @@ public class SendNewMailActivity extends DrawerBaseActivity {
                 }
             }
         }
+    }
+
+    private void fetchTemplates() {
+        Query query = db.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID))
+                .collection(Constants.KEY_COLLECTION_TEMPLATES)
+                .orderBy("createdAt", Query.Direction.ASCENDING);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    templatesList = new ArrayList<>(); // Initialize the list here
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Template template = document.toObject(Template.class);
+                        templatesList.add(template);
+                    }
+                    // Populate the dropdown
+                    Log.d("FetchTemplates", "Number of templates fetched: " + templatesList.size());
+                    if (!templatesList.isEmpty()) {
+                        populateDropdown();
+                    }
+                } else {
+                    // Handle errors
+                    Toast.makeText(SendNewMailActivity.this, "Failed to fetch templates", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void populateDropdown() {
+        List<String> templateNames = new ArrayList<>();
+        for (Template template : templatesList) {
+            templateNames.add(template.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, templateNames);
+        templateDropdown.setAdapter(adapter);
     }
 
 }
